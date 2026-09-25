410907bdb185bd69df809d1b504d65a5360d25a2
bhavesh-amrutkar-dev <bhavesh.amrutkar.dev@gmail.com>
2026-09-25 16:39:45 +0530

    Time: 4 ms (30.51%), Space: 8.7 MB (36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+        int initialVal = x;
+        long int revNum = 0;
+        while (x > 0) {
+            revNum = (revNum * 10) + (x % 10);
+            x /= 10;
+        }
+        if (initialVal == revNum) {
+            return true;
+        }
+        return false;
+    }
+};
